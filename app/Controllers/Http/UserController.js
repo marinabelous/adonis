00322673466b69b45394c9f31ca71df5f2468aac
teacher <teacher@stepdn.com>
2019-05-26 13:06:13 +0300

    first commit

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const User = use (`App/Models/User`)
+
+class UserController {
+
+    async index({request, response, view}) {
+        const users = await
+        User.all()
+        return view.render(`user.index`, {users: users.to JSON()})
+    }
+
+    async show({request, response, view, params}) {
+
+    }
+    async store({request, response}) {
+           const user = new User()
+           cost formData = request.all()
+            user.merge(formData)
+
+        await user.save()
+
+        return response.json(user)
+
+
+    }
+    async save({request, response, view, params}) {
+
+    }
+    async destroy({request, response, view, params}) {
+
+    }
+    async create({request, response, view, params}) {
+
+    }
+    async edit({request, response, view, params}) {
+
+    }
+}
+
+module.exports = UserController
---
+++ start/routes.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.on('/').render('welcome')
+
+//Users routes
+Route.get(`/users`, `UserController.index`).as(`users`)
+Route.post(`/users', UserController.store)
+Route.get('/users/:id, 'UserController.edit')
+Route.post('/users-save/:id, 'UserController.save')
+Route.get('/users-delete/:id, 'UserController.destroy')
+
